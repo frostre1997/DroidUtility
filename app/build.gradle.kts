adf65c0638d4445fa18b20a5750696979bec6697
Frostre_ <n9043395@gmail.com>
2026-07-05 15:27:46 +0200

    Add Compose plugin and update dependencies
    Added Compose plugin and updated dependencies for Compose.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose") // Necessario per le nuove versioni di Compose
 }
 
 android {
@@ -18,7 +19,7 @@ android {
     buildFeatures {
         compose = true
     }
-    
+
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.1"
     }
@@ -26,8 +27,11 @@ android {
 
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.compose.ui:ui:1.6.0")
-    implementation("androidx.compose.material3:material3:1.2.0")
+    implementation("androidx.activity:activity-compose:1.8.0")
+    implementation(platform("androidx.compose:compose-bom:2023.10.01"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.ui:ui-tooling-preview")
     
     implementation("dev.rikka.rikkax.shizuku:api:13.1.0")
     implementation("dev.rikka.rikkax.shizuku:provider:13.1.0")
