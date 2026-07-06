7b95793c7f0a4431ce88e71a496ce18e7a8b7bcf
Frostre_ <n9043395@gmail.com>
2026-07-06 17:36:44 +0200

    Format maven repository declaration in settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,7 +11,9 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven { url = uri("https://maven.rikka.dev/repository/maven-public/"} }
+        maven {
+            url = uri("https://maven.rikka.dev/repository/maven-public/")
+        }
     }
 }
 
