63c530200cc8e7ee928a85cfc29d1b6a4bcf2fce
Frostre_ <n9043395@gmail.com>
2026-07-06 16:36:52 +0200

    Add custom Maven repository for dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,6 +14,7 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://maven.rikka.dev/repository/maven-public/") }
         maven { url = uri("https://jitpack.io") }
     }
 }
