c6ad605e113c229075cd2838bf5f73588308ffe5
Frostre_ <n9043395@gmail.com>
2026-07-16 21:45:24 +0200

    Implement navigation graph with home and console tabs

--- app/src/main/java/com/frostre1997/droidutility/navigation/NavGraph.kt
+++ app/src/main/java/com/frostre1997/droidutility/navigation/NavGraph.kt
@@ -1,1 +1,32 @@
+package com.frostre1997.droidutility.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.frostre1997.droidutility.tabs.ConsoleTab
+import com.frostre1997.droidutility.tabs.HomeTab
+
+object Routes {
+    const val HOME = "home"
+    const val CONSOLE = "console"
+}
+
+@Composable
+fun NavGraph(navController: NavHostController) {
+    NavHost(
+        navController = navController,
+        startDestination = Routes.HOME
+    ) {
+        composable(Routes.HOME) {
+            HomeTab(
+                onOpenConsole = { navController.navigate(Routes.CONSOLE) }
+            )
+        }
+        composable(Routes.CONSOLE) {
+            ConsoleTab(
+                onBack = { navController.popBackStack() }
+            )
+        }
+    }
+}
