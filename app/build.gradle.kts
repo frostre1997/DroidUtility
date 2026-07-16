2a161298fc8208b34db242ffbc7151857110831f
Frostre_ <n9043395@gmail.com>
2026-07-16 18:18:05 +0200

    Add latest version of material3-window-size-class

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,6 +66,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
+    implementation("androidx.compose.material3:material3-window-size-class:<latest-version>")
 
     // Activity & Lifecycle
     implementation("androidx.activity:activity-compose:1.8.2")
