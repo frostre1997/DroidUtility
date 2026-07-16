010b3a8d67675200ba81dd22c3acfe1176302280
Frostre_ <n9043395@gmail.com>
2026-07-16 12:56:09 +0200

    Update dependency declaration syntax in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,7 +54,7 @@ android {
 
 dependencies {
     implementation("dev.rikka.shizuku:api:13.1.5")
-    implementation 'dev.rikka.shizuku:provider:13.1.5'
+    implementation ("dev.rikka.shizuku:provider:13.1.5")
 
     // Compose BOM
     val composeBom = platform("androidx.compose:compose-bom:2024.02.00")
