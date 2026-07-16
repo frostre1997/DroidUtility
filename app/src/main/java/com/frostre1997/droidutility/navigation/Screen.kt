4b8c5b6e2ec231030252c31f9851d0f361161757
Frostre_ <n9043395@gmail.com>
2026-07-16 18:05:19 +0200

    Add sealed class Screen with navigation routes

--- app/src/main/java/com/frostre1997/droidutility/navigation/Screen.kt
+++ app/src/main/java/com/frostre1997/droidutility/navigation/Screen.kt
@@ -1,1 +1,9 @@
+package com.frostre1997.droidutility.navigation
 
+sealed class Screen(val route: String) {
+    data object Home : Screen("home")
+    data object Terminal : Screen("terminal")
+    data object Debloat : Screen("debloat")
+    data object Console : Screen("console")
+    data object Settings : Screen("settings")
+}
