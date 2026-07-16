a90f779d7ae7f11762fb8f1894f70f3fae175ae3
Frostre_ <n9043395@gmail.com>
2026-07-16 21:46:51 +0200

    Create Color.kt with theme color values
    Added color definitions for the app theme.

---
+++ app/src/main/java/com/frostre1997/droidutility/ui/themes/Color.kt
@@ -0,0 +1,19 @@
+package com.frostre1997.droidutility.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650A4)
+val PurpleGrey40 = Color(0xFF625B71)
+val Pink40 = Color(0xFF7D5260)
+
+val BackgroundDark = Color(0xFF101114)
+val SurfaceDark = Color(0xFF1A1B20)
+val AccentBlue = Color(0xFF4D7CFE)
+val AccentGreen = Color(0xFF42C98C)
+val AccentRed = Color(0xFFFF5C77)
+val TextPrimary = Color(0xFFEDEEF2)
+val TextSecondary = Color(0xFFA7AAB3)
