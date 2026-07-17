2d7b8213b848cb6103ae425d97049b3c9a35fcde
Frostre_ <n9043395@gmail.com>
2026-07-17 22:57:31 +0200

    Change import from BloatRisk to RiskLevel
    Updated import statement to use RiskLevel instead of BloatRisk.

--- app/src/main/java/com/frostre1997/droidutility/bloat/HuaweiBloat.kt
+++ app/src/main/java/com/frostre1997/droidutility/bloat/HuaweiBloat.kt
@@ -2,7 +2,7 @@ package com.frostre1997.droidutility.bloat
 
 import com.frostre1997.droidutility.BloatApp
 import com.frostre1997.droidutility.BloatCategory
-import com.frostre1997.droidutility.BloatRisk
+import com.frostre1997.droidutility.RiskLevel
 
 object HuaweiBloat {
     val BLOATWARE = listOf(
