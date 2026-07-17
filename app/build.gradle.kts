1a1c68f06c2d4d5e116c76552b80ea0cb8901267
Frostre_ <n9043395@gmail.com>
2026-07-17 12:52:19 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,6 +58,10 @@ dependencies {
 
     // Compose BOM
     val composeBom = platform("androidx.compose:compose-bom:2024.02.00")
+    implementation 'androidx.compose.ui:ui'
+    implementation 'androidx.compose.material3:material3'
+    implementation 'androidx.compose.foundation:foundation'
+    implementation 'androidx.compose.ui:ui-tooling-preview'
     implementation(composeBom)
 
     // Compose UI
@@ -67,6 +71,8 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.compose.material3:material3-window-size-class:<latest-version>")
+    
+    // Navigation Compose
     implementation("androidx.navigation:navigation-compose:2.7.7")
 
     // Activity & Lifecycle
