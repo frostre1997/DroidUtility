6f407d0dd57abce07ac4a713fcb155897d9ac7f0
Frostre_ <n9043395@gmail.com>
2026-07-17 12:55:12 +0200

    Add BloatCategory enum for categorizing bloatware

---
+++ app/src/main/java/com/frostre1997/droidutility/data/data/BloatCategory.kt
@@ -0,0 +1,12 @@
+package com.frostre1997.droidutility.data
+
+enum class BloatCategory {
+    SYSTEM,
+    GOOGLE,
+    PRIVACY_CONCERNING,
+    CARRIER,
+    OEM,
+    SOCIAL,
+    GAME,
+    OTHER
+}
