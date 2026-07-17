b84d476777e17b9c38453d12721a4ea7c5ecb289
Frostre_ <n9043395@gmail.com>
2026-07-17 16:43:42 +0200

    Update BloatList.kt

--- app/src/main/java/com/frostre1997/droidutility/BloatList.kt
+++ app/src/main/java/com/frostre1997/droidutility/BloatList.kt
@@ -1,7 +1,19 @@
 package com.frostre1997.droidutility
 
+import com.frostre1997.droidutility.bloat.GoogleBloat
+import com.frostre1997.droidutility.bloat.HuaweiBloat
+import com.frostre1997.droidutility.bloat.LenovoBloat
+import com.frostre1997.droidutility.bloat.MotorolaBloat
+import com.frostre1997.droidutility.bloat.OnePlusBloat
+import com.frostre1997.droidutility.bloat.OppoBloat
+import com.frostre1997.droidutility.bloat.RealmeBloat
+import com.frostre1997.droidutility.bloat.SamsungBloat
+import com.frostre1997.droidutility.bloat.VivoBloat
+import com.frostre1997.droidutility.bloat.XiaomiBloat
+import com.frostre1997.droidutility.data.BloatApp
+
 object BloatList {
-    val ALL = buildList {
+    val ALL: List<BloatApp> = buildList {
         addAll(SamsungBloat.BLOATWARE)
         addAll(XiaomiBloat.BLOATWARE)
         addAll(OnePlusBloat.BLOATWARE)
