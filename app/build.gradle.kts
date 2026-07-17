d4699710f23c2cd45e812452967b3e0d8e97b64f
Frostre_ <n9043395@gmail.com>
2026-07-17 13:08:13 +0200

    Update Compose dependencies to use parentheses

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,10 +58,10 @@ dependencies {
 
     // Compose BOM
     val composeBom = platform("androidx.compose:compose-bom:2024.02.00")
-    implementation 'androidx.compose.ui:ui'
-    implementation 'androidx.compose.material3:material3'
-    implementation 'androidx.compose.foundation:foundation'
-    implementation 'androidx.compose.ui:ui-tooling-preview'
+    implementation ("androidx.compose.ui:ui")
+    implementation ("androidx.compose.material3:material3")
+    implementation ("androidx.compose.foundation:foundation")
+    implementation ("androidx.compose.ui:ui-tooling-preview")
     implementation(composeBom)
 
     // Compose UI
