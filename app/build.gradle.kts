69eea5a51199efee89dc150a141bab8dcf139030
Frostre_ <n9043395@gmail.com>
2026-07-17 10:03:45 +0200

    Add navigation-compose dependency to build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,6 +67,7 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.compose.material3:material3-window-size-class:<latest-version>")
+    implementation("androidx.navigation:navigation-compose:2.7.7")
 
     // Activity & Lifecycle
     implementation("androidx.activity:activity-compose:1.8.2")
