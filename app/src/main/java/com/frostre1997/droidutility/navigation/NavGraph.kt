367bd7c8079acde238725f0a014bd5db5e0974aa
Frostre_ <n9043395@gmail.com>
2026-07-17 12:53:15 +0200

    Refactor NavGraph to use new screens and routes

--- app/src/main/java/com/frostre1997/droidutility/navigation/NavGraph.kt
+++ app/src/main/java/com/frostre1997/droidutility/navigation/NavGraph.kt
@@ -4,29 +4,15 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import com.frostre1997.droidutility.tabs.ConsoleTab
-import com.frostre1997.droidutility.tabs.HomeTab
-
-object Routes {
-    const val HOME = "home"
-    const val CONSOLE = "console"
-}
+import androidx.navigation.compose.rememberNavController
+import com.frostre1997.droidutility.ui.screens.DebloatScreen
+import com.frostre1997.droidutility.ui.screens.TerminalScreen
 
 @Composable
-fun NavGraph(navController: NavHostController) {
-    NavHost(
-        navController = navController,
-        startDestination = Routes.HOME
-    ) {
-        composable(Routes.HOME) {
-            HomeTab(
-                onOpenConsole = { navController.navigate(Routes.CONSOLE) }
-            )
-        }
-        composable(Routes.CONSOLE) {
-            ConsoleTab(
-                onBack = { navController.popBackStack() }
-            )
-        }
+fun NavGraph(navController: NavHostController = rememberNavController()) {
+    NavHost(navController = navController, startDestination = "debloat") {
+        composable("debloat") { DebloatScreen() }
+        composable("terminal") { TerminalScreen() }
+        // add other routes
     }
 }
