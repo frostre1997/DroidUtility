f4d5ad6e0db5636aea1ad69e9293728534e072b5
Frostre_ <n9043395@gmail.com>
2026-07-18 19:10:33 +0200

    Refactor MainActivity to include navigation

--- app/src/main/java/com/frostre1997/droidutility/MainActivity.kt
+++ app/src/main/java/com/frostre1997/droidutility/MainActivity.kt
@@ -3,8 +3,16 @@ package com.frostre1997.droidutility
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.frostre1997.droidutility.ui.screens.DebloatScreen
+import com.frostre1997.droidutility.ui.screens.TerminalScreen
 import com.frostre1997.droidutility.ui.theme.DroidUtilityTheme
 
 class MainActivity : ComponentActivity() {
@@ -12,10 +20,27 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             DroidUtilityTheme {
-                Surface(color = MaterialTheme.colorScheme.background) {
-                    DroidUtilityApp()
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    AppNavigation()
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun AppNavigation() {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "debloat") {
+        composable("debloat") {
+            DebloatScreen(onBack = { navController.popBackStack() })
+        }
+        composable("terminal") {
+            TerminalScreen(onBack = { navController.popBackStack() })
         }
     }
 }
