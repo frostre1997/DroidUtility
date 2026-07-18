84207a8478c1588a708804a9c60f44bb73b05b92
Frostre_ <n9043395@gmail.com>
2026-07-18 12:39:52 +0200

    Refactor BloatList imports and update BLOATWARE list

--- app/src/main/java/com/frostre1997/droidutility/ui/components/BloatList.kt
+++ app/src/main/java/com/frostre1997/droidutility/ui/components/BloatList.kt
@@ -1,29 +1,27 @@
-package com.frostre1997.droidutility
+package com.frostre1997.droidutility.ui.components
 
+import com.frostre1997.droidutility.data.BloatApp
 import com.frostre1997.droidutility.bloat.GoogleBloat
 import com.frostre1997.droidutility.bloat.HuaweiBloat
 import com.frostre1997.droidutility.bloat.LenovoBloat
 import com.frostre1997.droidutility.bloat.MotorolaBloat
-import com.frostre1997.droidutility.bloat.OnePlusBloat
 import com.frostre1997.droidutility.bloat.OppoBloat
 import com.frostre1997.droidutility.bloat.RealmeBloat
 import com.frostre1997.droidutility.bloat.SamsungBloat
 import com.frostre1997.droidutility.bloat.VivoBloat
 import com.frostre1997.droidutility.bloat.XiaomiBloat
-import com.frostre1997.droidutility.data.BloatApp
-import com.frostre1997.droidutility.data.BloatCategory
 
 object BloatList {
     val ALL: List<BloatApp> = buildList {
         addAll(SamsungBloat.BLOATWARE)
         addAll(XiaomiBloat.BLOATWARE)
         addAll(OnePlusBloat.BLOATWARE)
-        addAll(HuaweiBloat.BLOATWARE)
         addAll(OppoBloat.BLOATWARE)
         addAll(VivoBloat.BLOATWARE)
         addAll(RealmeBloat.BLOATWARE)
+        addAll(HuaweiBloat.BLOATWARE)
+        addAll(LenovoBloat.BLOATWARE)
         addAll(MotorolaBloat.BLOATWARE)
         addAll(GoogleBloat.BLOATWARE)
-        addAll(LenovoBloat.BLOATWARE)
     }
 }
