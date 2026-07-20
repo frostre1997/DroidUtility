c7a1a79f1f6529ba8535ac8d8115e18e43930b4f
Frostre_ <n9043395@gmail.com>
2026-07-20 09:42:07 +0200

    Update DroidUtilityTheme usage in MainActivity

--- app/src/main/java/com/frostre1997/droidutility/MainActivity.kt
+++ app/src/main/java/com/frostre1997/droidutility/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            DroidUtilityTheme {
+            DroidUtilityTheme { // no extra parameters for now (default AMOLED)
                 Surface(modifier = Modifier.fillMaxSize()) {
                     MainScreen()
                 }
