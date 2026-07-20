aff81c5c81fb53210eee01565b049916dc85707f
Frostre_ <n9043395@gmail.com>
2026-07-20 09:31:10 +0200

    Add ShellScreen composable for shell UI

---
+++ app/src/main/java/com/frostre1997/droidutility/ui/screens/ShellScreen.kt
@@ -0,0 +1,29 @@
+package com.frostre1997.droidutility.ui.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ShellScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black)
+            .padding(24.dp)
+    ) {
+        Text(
+            text = "Shell",
+            style = MaterialTheme.typography.headlineLarge,
+            color = Color.White
+        )
+        // Add your shell UI here
+    }
+}
