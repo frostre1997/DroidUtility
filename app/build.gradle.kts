0012986bce3bb0698041db4d4b4c20c3906e230c
Frostre_ <n9043395@gmail.com>
2026-07-20 09:21:48 +0200

    Add Material Components library dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -106,6 +106,7 @@ dependencies {
     implementation("androidx.compose.foundation:foundation")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
+    implementation("com.google.android.material:material:1.12.0")
 
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("androidx.activity:activity-compose:1.8.2")
