7f4672121f1ceae9dfbeed1069add5a7b2bbc2db
Frostre_ <n9043395@gmail.com>
2026-07-20 12:46:00 +0200

    Integrate settingsManager for theme management
    Initialize settingsManager and integrate theme settings from it.

--- app/src/main/java/com/frostre1997/droidutility/MainActivity.kt
+++ app/src/main/java/com/frostre1997/droidutility/MainActivity.kt
@@ -9,10 +9,22 @@ import androidx.compose.ui.Modifier
 import com.frostre1997.droidutility.ui.theme.DroidUtilityTheme
 
 class MainActivity : ComponentActivity() {
+    private lateinit var settingsManager: SettingsManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        settingsManager = SettingsManager(this)
+
         setContent {
-            DroidUtilityTheme {
+            val themeModeFlow = settingsManager.getThemeModeFlow().collectAsState(initial = "AMOLED")
+            val dynamicColorFlow = settingsManager.getDynamicColorFlow().collectAsState(initial = false)
+            val themeMode = ThemeMode.valueOf(themeModeFlow.value)
+            val useDynamicColor = dynamicColorFlow.value
+
+            DroidUtilityTheme(
+                themeMode = themeMode,
+                useDynamicColor = useDynamicColor
+            ) {
                 Surface(modifier = Modifier.fillMaxSize()) {
                     MainScreen()
                 }
